146d8d1b0118c035c242d8a68056c93be95f5804
Yufei Miao <yufeim@yufeim.com>
2023-09-30 17:32:01 -0500

    add production server

---
+++ frontend/production/index.js
@@ -0,0 +1,46 @@
+import fs from "fs";
+import http from "http";
+import path from "path";
+import process from "process";
+import { fileURLToPath } from "url";
+import express from "express";
+
+const application_path = path.resolve(
+  path.dirname(fileURLToPath(import.meta.url)) + "/../dist"
+);
+const app = express();
+
+app.use(express.static(application_path));
+app.use((req, res) => {
+  res.sendFile(application_path + "/index.html");
+});
+
+let listen_address;
+if (process.env.SOCK != undefined) {
+  listen_address = process.env.SOCK;
+  if (fs.existsSync(listen_address)) {
+    fs.unlinkSync(listen_address);
+  }
+} else {
+  listen_address = process.env.PORT || 8080;
+}
+
+const server = http.createServer(app);
+
+process.on("SIGINT", () => {
+  server.close();
+  if (fs.existsSync(listen_address)) {
+    fs.unlinkSync(listen_address);
+  }
+  process.exit(0);
+});
+
+console.log("CSE521S Frontend");
+
+server.listen(listen_address, (error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server listening on " + listen_address);
+  }
+});
