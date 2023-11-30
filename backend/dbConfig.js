986f8b7093a2618dcded57014496ede0f59b3d68
Yufei Miao <m.yufei@wustl.edu>
2023-11-30 17:32:00 -0600

    add prod db settings

--- backend/dbConfig.js
+++ backend/dbConfig.js
@@ -1,6 +1,23 @@
 import mysql from 'mysql2/promise';
 
-const dbconn = mysql.createPool({
+let production = false;
+
+if (process.env.ENV) {
+  if (process.env.ENV === "production") {
+    production = true;
+  }
+}
+
+const dbconn =
+production ?
+mysql.createPool({
+  host: 'cse521s.clkofmhgqjwu.us-east-2.rds.amazonaws.com',
+  user: 'cse521s',
+  password: 'Mypass521',
+  database: 'cse521s'
+})
+:
+mysql.createPool({
   host: 'localhost',
   user: 'cse521s',
   password: 'Mypass521',
